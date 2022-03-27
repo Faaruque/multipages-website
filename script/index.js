0a157b871e954aa6d35c6c831f57d0b2295be60a
Faaruque <faaruque99@gmail.con>
2022-03-27 23:53:25 +0600

    initil commit

---
+++ script/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 1) {
+      $(".navBar").addClass("sticky");
+    } else {
+      $(".navBar").removeClass("sticky");
+    }
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 300) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+});
